a3361144104801081f4f575b4a31a921c1ff6eca
Daniel Couso <daniel.couso@liferay.com>
2020-03-20 15:10:05 +0100

    LPS-109995 Remove unncessary configuration for text editables

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/editor/configuration/FragmentEntryLinkEditorConfigContributor.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/editor/configuration/FragmentEntryLinkEditorConfigContributor.java
@@ -14,12 +14,6 @@
 
 package com.liferay.layout.content.page.editor.web.internal.editor.configuration;
 
-import com.liferay.item.selector.ItemSelector;
-import com.liferay.item.selector.ItemSelectorCriterion;
-import com.liferay.item.selector.criteria.DownloadURLItemSelectorReturnType;
-import com.liferay.item.selector.criteria.URLItemSelectorReturnType;
-import com.liferay.item.selector.criteria.image.criterion.ImageItemSelectorCriterion;
-import com.liferay.item.selector.criteria.url.criterion.URLItemSelectorCriterion;
 import com.liferay.layout.content.page.editor.constants.ContentPageEditorPortletKeys;
 import com.liferay.portal.kernel.editor.configuration.BaseEditorConfigContributor;
 import com.liferay.portal.kernel.editor.configuration.EditorConfigContributor;
@@ -27,14 +21,10 @@ import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.GetterUtil;
 
 import java.util.Map;
 
-import javax.portlet.PortletURL;
-
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Pavel Savinov
@@ -58,16 +48,6 @@ public class FragmentEntryLinkEditorConfigContributor
 		ThemeDisplay themeDisplay,
 		RequestBackedPortletURLFactory requestBackedPortletURLFactory) {
 
-		String namespace = GetterUtil.getString(
-			inputEditorTaglibAttributes.get(
-				"liferay-ui:input-editor:namespace"));
-		String name = GetterUtil.getString(
-			inputEditorTaglibAttributes.get("liferay-ui:input-editor:name"));
-
-		PortletURL itemSelectorURL = _itemSelector.getItemSelectorURL(
-			requestBackedPortletURLFactory, namespace + name + "selectItem",
-			getImageItemSelectorCriterion(), getURLItemSelectorCriterion());
-
 		jsonObject.put(
 			"allowedContent", ""
 		).put(
@@ -79,10 +59,6 @@ public class FragmentEntryLinkEditorConfigContributor
 		).put(
 			"extraPlugins", getExtraPluginsLists()
 		).put(
-			"filebrowserImageBrowseLinkUrl", itemSelectorURL.toString()
-		).put(
-			"filebrowserImageBrowseUrl", itemSelectorURL.toString()
-		).put(
 			"removePlugins", getRemovePluginsLists()
 		).put(
 			"toolbars", JSONFactoryUtil.createJSONObject()
@@ -92,35 +68,11 @@ public class FragmentEntryLinkEditorConfigContributor
 	protected String getExtraPluginsLists() {
 		return "ae_autolink,ae_dragresize,ae_addimages,ae_imagealignment," +
 			"ae_placeholder,ae_selectionregion,ae_tableresize," +
-				"ae_tabletools,ae_uicore,itemselector,media,adaptivemedia";
-	}
-
-	protected ItemSelectorCriterion getImageItemSelectorCriterion() {
-		ItemSelectorCriterion itemSelectorCriterion =
-			new ImageItemSelectorCriterion();
-
-		itemSelectorCriterion.setDesiredItemSelectorReturnTypes(
-			new DownloadURLItemSelectorReturnType());
-
-		return itemSelectorCriterion;
+				"ae_tabletools,ae_uicore";
 	}
 
 	protected String getRemovePluginsLists() {
 		return "contextmenu,elementspath,floatingspace,image,link,liststyle," +
 			"magicline,resize,tabletools,toolbar,ae_embed";
 	}
-
-	protected ItemSelectorCriterion getURLItemSelectorCriterion() {
-		ItemSelectorCriterion itemSelectorCriterion =
-			new URLItemSelectorCriterion();
-
-		itemSelectorCriterion.setDesiredItemSelectorReturnTypes(
-			new URLItemSelectorReturnType());
-
-		return itemSelectorCriterion;
-	}
-
-	@Reference
-	private ItemSelector _itemSelector;
-
 }
