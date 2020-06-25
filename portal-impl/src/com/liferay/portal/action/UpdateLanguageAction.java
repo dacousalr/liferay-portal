895910562972e5c750b8bd70c0f61a1d4e5d30fd
balazssk <balazs.safrany-kovalik@liferay.com>
2020-06-25 10:24:29 +0200

    LPS-109019 Follow the pattern of how Layout URLs are changed before redirect

--- portal-impl/src/com/liferay/portal/action/UpdateLanguageAction.java
+++ portal-impl/src/com/liferay/portal/action/UpdateLanguageAction.java
@@ -23,7 +23,9 @@ import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.FriendlyURLResolverRegistryUtil;
+import com.liferay.portal.kernel.portlet.LayoutFriendlyURLSeparatorComposite;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
@@ -37,6 +39,7 @@ import com.liferay.portal.util.PropsValues;
 import com.liferay.portlet.admin.util.AdminUtil;
 
 import java.util.Locale;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -121,9 +124,27 @@ public class UpdateLanguageAction implements Action {
 		int posFriendlyURLSeparator = layoutURL.indexOf(
 			Portal.FRIENDLY_URL_SEPARATOR);
 
+		Layout layout = themeDisplay.getLayout();
+
 		if (posFriendlyURLSeparator != -1) {
 			friendlyURLSeparatorPart = layoutURL.substring(
 				posFriendlyURLSeparator);
+
+			Map<String, Object> requestContext =
+				HashMapBuilder.<String, Object>put(
+					"request", httpServletRequest
+				).build();
+
+			LayoutFriendlyURLSeparatorComposite
+				layoutFriendlyURLSeparatorComposite =
+					PortalUtil.getLayoutFriendlyURLSeparatorComposite(
+						layout.getGroupId(), layout.isPrivateLayout(),
+						friendlyURLSeparatorPart,
+						httpServletRequest.getParameterMap(), requestContext);
+
+			friendlyURLSeparatorPart =
+				layoutFriendlyURLSeparatorComposite.getFriendlyURL();
+
 			layoutURL = layoutURL.substring(0, posFriendlyURLSeparator);
 		}
 
@@ -134,8 +155,6 @@ public class UpdateLanguageAction implements Action {
 				layoutURL = layoutURL.substring(i18nPath.length());
 			}
 		}
-
-		Layout layout = themeDisplay.getLayout();
 
 		if (isFriendlyURLResolver(layoutURL) || layout.isTypeControlPanel()) {
 			redirect = layoutURL + friendlyURLSeparatorPart;
